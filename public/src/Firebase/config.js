ff98a23b9c0c5929527c4de220c62d6cb5d4849e
LyHoangViet <hviet519@gmail.com>
2025-03-21 14:31:10 +0700

    Revert "abc"
    This reverts commit a196098c5fcd1c82bc3b0ab4e498fdfe5124097d.

--- public/src/Firebase/config.js
+++ public/src/Firebase/config.js
@@ -1,8 +1,14 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth, GoogleAuthProvider, FacebookAuthProvider } from "firebase/auth";
+import { getAuth } from "firebase/auth";
+import { GoogleAuthProvider, FacebookAuthProvider } from "firebase/auth";
 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -13,11 +19,11 @@ const firebaseConfig = {
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-const analytics = typeof window !== 'undefined' ? getAnalytics(app) : null;
-const auth = getAuth(app);
-const ggProvider = new GoogleAuthProvider();
-const fbProvider = new FacebookAuthProvider();
+const analytics = getAnalytics();
+const auth = getAuth();
+const ggProvider = new GoogleAuthProvider(app);
+const fbProvider = new FacebookAuthProvider(app);
 
 export { app, analytics, auth, ggProvider, fbProvider };
